b7e0c8a4ca45aad4bcfa957e1ce8289585362a85
Xushnid <xmamtqosimov@gmail.com>
2026-09-11 15:22:10 +0900

    F task bajarildi

--- run.py
+++ run.py
@@ -1,2 +1,18 @@
-message = "Hello world";
-print(message);
+message = "Hello world"
+print(message)
+
+#  Mit Task
+
+
+def findDoublers(text):
+    for letter in text:
+        if text.count(letter) == 2:
+            return True
+    return False;
+        
+            
+    
+
+
+res = findDoublers("halol")
+print(res)
