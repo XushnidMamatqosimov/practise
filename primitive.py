d4d2f0727116f949ed7f12498da9cd7ee29d2f32
Xushnid <xmamtqosimov@gmail.com>
2026-09-12 01:21:34 +0900

    feat: learn python boolean

--- primitive.py
+++ primitive.py
@@ -28,4 +28,20 @@ res = course.replace("FullStack", "MasterClass")
 print(f"the result: {res}")
 
 print("======== Boolean =========")
+# functions: => type(), input(), bool(), int(), str()
+y = input("Give your value for Y: ")
+print("value of Y: ", y)
+
+res1 = y.isnumeric()
+print(f"the input value is numeric: {res1}")
+
+# TRUSY vs FALSY value
+# TRUSY: True, 100, -100, "MIT"
+# FALSY: False, 0, "", None
+
+test_falsy = "" or False or None or 100
+print("The test_falsy: ", bool(test_falsy))
+
+test_trusy= " MIR " 
+print("The test_trusy: ", bool(test_trusy))
  
