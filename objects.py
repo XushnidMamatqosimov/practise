f017e0310f8f0e98ae215974dea62bfd3b564dae
Xushnid <xmamtqosimov@gmail.com>
2026-09-15 15:10:24 +0900

    feat: Error handling

--- objects.py
+++ objects.py
@@ -5,7 +5,7 @@
  (4) Error handling system
 '''
 
-import array # package
+import array  # package
 import math
 from math import ceil
 print("===== What is Object =====")
@@ -20,3 +20,17 @@ print(type(math))
 print(math.ceil(97.7))
 print(math.ceil(98.7))
 
+
+print("===== Error Handling =====")
+car_dict = dict(model="Toyota", year=2026, electro=True)
+
+try:
+    print("passed here")
+    a = car_dict.speed
+    print(f"result: {car_dict['origin']}")
+except KeyError and AttributeError as err:
+    print("Error occured: ", err)
+else:
+    print("If try works properly we overflow except and print else together with try")
+finally:
+    print("Finally always work no metter how was the result above")
