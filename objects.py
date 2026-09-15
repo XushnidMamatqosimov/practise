563e41a753e27d7be8f356a67af8f6831e49c7bb
Xushnid <xmamtqosimov@gmail.com>
2026-09-15 13:53:42 +0900

    feat: Learn python objects

---
+++ objects.py
@@ -0,0 +1,23 @@
+''' Objects
+ (1) What is object
+ (2) Iterable objects  & Range
+ (3) Dictionary 
+ (4) Error handling system
+'''
+
+import array # package
+import math
+from math import ceil
+print("===== What is Object =====")
+# An object has state and method properties
+# Everything is object in python
+print(type("Hello World"))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+print(math.ceil(97.7))
+print(math.ceil(98.7))
+
+
